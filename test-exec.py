91d3ccb1913a9ebb054ae6503b54e4a309915c46
landinij0040 <ijlandin0040@miners.utep.edu>
2021-01-31 18:05:42 -0700

    Added test-exec.py

--- test-exec.py
+++ test-exec.py
@@ -1,11 +1,10 @@
-# print("Files in %r: %s" % (cwd, files))
-import sys
 import os
-if __name__ == '__main__':
-    # cwd = os.getcwd() # Get the current working directory
-    # files = os.listdir(cwd)
-
-    print("This is the name of the other script: ", sys.argv[1])
-
-
-    os.execv('helloworld.py',sys.argv [1:])
+p=os.getcwd()
+args=[p+'/helloworld.py']
+cpid=os.fork()
+if cpid == 0:
+    print('Child')
+    os.execv('/usr/bin/python3',['python3']+args)
+else:
+    print('Parent')
+    os.waitpid(0,0)
