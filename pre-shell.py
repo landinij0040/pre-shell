e0ab83ec293b2c949a87a360c2269f5857783c09
landinij0040 <ijlandin0040@miners.utep.edu>
2021-01-31 17:50:45 -0700

    exec finaly works

--- pre-shell.py
+++ pre-shell.py
@@ -1,13 +1,12 @@
 import os
+import sys
 
 if __name__ == "__main__":
     n = os.fork()
-
     # n greater than 0 means parent process
     if n > 0:
         print("Parent process and id is : ", os.getpid())
-    
     # n equals to 0 means child process
     else:
-        print("Child process and is is : ", os.getpid())
-    
+        print("Child process is : ", os.getpid())
+        os.execv('/usr/bin/python3', ['python3'] + [os.getcwd()+'/helloworld.py'])
